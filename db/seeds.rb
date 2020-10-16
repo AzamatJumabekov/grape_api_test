6e01488e0f7d46396ddb537a81f7b93b070a9a68
Azamat Jumabekov <azamat.jumabekov@gmail.com>
2020-10-17 01:21:11 +0600

    Refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,62 @@
 require 'typhoeus'
 
-def entities_to_create(model, max_count)
-  current = (max_count - model.count)
-  current >= 0 ? current : 0
+def hydra_instance
+  Typhoeus::Hydra.new(max_concurrency: ENV.fetch("RAILS_MAX_THREADS") { 40 })
 end
 
-# entities_to_create(User, 100).times do
-#   User.create(login: SecureRandom.hex(4))
-# end
-
-users = User.all
-
-hydra = Typhoeus::Hydra.new(max_concurrency: ENV.fetch("RAILS_MAX_THREADS") { 40 })
-entities_to_create(Post, 200_000).times do
-  params = {
-    title: SecureRandom.hex(4),
-    ip: "255.255.255.#{rand(1..50)}",
-    content: SecureRandom.hex(50).scan(/.{1,6}/).join(' '),
-    author: users.sample.login
-  }
-
+def add_to_queue(hydra, endpoint, params)
+  url = 'localhost:9292'
+  full_url = url + endpoint
   hydra.queue(
     Typhoeus::Request.new(
-      "localhost:9292/api/v1/posts",
+      full_url,
       followlocation: true,
       body: params,
       method: :post
     )
   )
 end
-hydra.run
-# post_ids = Post.pluck(:id)
 
-# entities_to_create(Rating, 200_000).times do
-#   ::Ratings::Create.new({value: rand(1..5), post_id: post_ids.sample}).call
-# end
+def entities_to_create(model, max_count)
+  current = (max_count - model.count)
+  current >= 0 ? current : 0
+end
+
+def create_users
+  hydra = hydra_instance
+  entities_to_create(User, 100).times do
+    params = { login: SecureRandom.hex(4) }
+    add_to_queue(hydra, '/api/v1/users', params)
+  end
+  hydra.run
+end
+
+
+def create_posts
+  users = User.all
+  hydra = hydra_instance
+  entities_to_create(Post, 200_000).times do
+    params = {
+      title: SecureRandom.hex(4),
+      ip: "255.255.255.#{rand(1..50)}",
+      content: SecureRandom.hex(50).scan(/.{1,6}/).join(' '),
+      author: users.sample.login
+    }
+    add_to_queue(hydra, '/api/v1/posts', params)
+  end
+  hydra.run
+end
+
+def create_ratings
+  hydra = hydra_instance
+  post_ids = Post.pluck(:id)
+  entities_to_create(Rating, 100_000).times do
+    params = { value: rand(1..5), post_id: post_ids.sample }
+    add_to_queue(hydra, '/api/v1/ratings', params)
+  end
+  hydra.run
+end
+
+create_users
+create_posts
+create_ratings
